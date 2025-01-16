38cce166b64b7bdcc205d3f1f84611d13881b76d
parthdev99 <105319463+parthdev99@users.noreply.github.com>
2025-01-17 00:57:32 +0100

    Update cstyle.py

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -5,6 +5,7 @@ from tinygrad.ops import GroupOp, Ops, UOp, PatternMatcher, UPat
 from tinygrad.helpers import strip_parens, getenv, prod, dedup, AMX
 from tinygrad.dtype import ImageDType, dtypes, DType, PtrDType
 from tinygrad.renderer import Renderer, TensorCore
+from builtins import *
 
 base_rewrite = PatternMatcher([
   (UPat(Ops.DEFINE_ACC, name="x"), lambda ctx,x: ctx[x.src[0]]),
